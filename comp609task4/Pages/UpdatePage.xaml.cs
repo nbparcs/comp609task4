34b44d4c84edcffe36cd7e40016911a10fb5e57f
Prabhdeep101 <prabhdeepsachdeva101@gmail.com>
2024-05-25 14:23:25 +1200

    Update Page UI completed, code behind is not yet written

--- comp609task4/Pages/UpdatePage.xaml.cs
+++ comp609task4/Pages/UpdatePage.xaml.cs
@@ -2,8 +2,14 @@ namespace comp609task4.Pages;
 
 public partial class UpdatePage : ContentPage
 {
-	public UpdatePage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<string> UpdateRecord { get; set; }
+    public UpdatePage()
+    {
+        InitializeComponent();
+    }
+
+    private void UpdateRecord_Btn(object sender, EventArgs e)
+    {
+
+    }
 }
