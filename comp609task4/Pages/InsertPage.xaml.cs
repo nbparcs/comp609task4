cccf9c4ac614606314d334f6da337bff4b3ff81c
Prabhdeep101 <prabhdeepsachdeva101@gmail.com>
2024-05-25 14:16:07 +1200

    Insert Page UI Created. Will add the code behind later on, this is just the UI to start with.

--- comp609task4/Pages/InsertPage.xaml.cs
+++ comp609task4/Pages/InsertPage.xaml.cs
@@ -2,8 +2,14 @@ namespace comp609task4.Pages;
 
 public partial class InsertPage : ContentPage
 {
-	public InsertPage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<string> InsertRecord { get; set; }
+    public InsertPage()
+    {
+        InitializeComponent();
+    }
+
+    private void InsertRecord_Btn(object sender, EventArgs e)
+    {
+
+    }
 }
