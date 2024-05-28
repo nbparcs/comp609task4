48171b4e0878d844a19d6ab84af7f105007da135
Prabhdeep101 <prabhdeepsachdeva101@gmail.com>
2024-05-28 14:26:07 +1200

    Headings for the DataPage

--- comp609task4/MauiProgram.cs
+++ comp609task4/MauiProgram.cs
@@ -16,10 +16,15 @@ namespace comp609task4
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
             builder.Services.AddSingleton<MainViewModel>();
             builder.Services.AddTransient<DataPage>();
+            builder.Services.AddTransient<StatsnInvestmentPage>();
+            builder.Services.AddTransient<InfoPage>();
+            builder.Services.AddTransient<InsertPage>();
+            builder.Services.AddTransient<DeletePage>();
+            builder.Services.AddTransient<UpdatePage>();
             return builder.Build();
         }
     }
